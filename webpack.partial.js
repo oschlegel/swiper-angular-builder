b6c8c8f3fda92d163bddaedc306344d55b26c440
Oliver Schlegel <oliver.schlegel@aracom.de>
2019-10-20 17:12:46 +0200

    chore: es6 swiper works !!!

--- webpack.partial.js
+++ webpack.partial.js
@@ -3,11 +3,11 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components|node_modules\/(?!(dom7|ssr-window|swiper)\/).*)/,
+        exclude: /node_modules\/(?!(dom7|ssr-window|swiper)\/).*/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env']
+            presets: ['@babel/preset-env']
           }
         }
       }
